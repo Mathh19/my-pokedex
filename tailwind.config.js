61aa26a4274ec5ab98c1117faa70a9a4ce5e6789
Matheus <mf635097@gmail.com>
2023-07-27 21:14:21 -0300

    feat: add pallet color according to pokemon type and config domain to image

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,31 @@ module.exports = {
       colors: {
         'bg-primary': '#191919',
         'text-primary': '#FAFAFA',
-        'nav-primary': '#E3350D'
+        'nav-primary': '#E3350D',
+        'pokemon-types': {
+          normal: '#9fa39d',
+          fighting: '#ff215b',
+          flying: '#89bdff',
+          poison: '#f149ff',
+          ground: '#ff6b0d',
+          rock: '#d8bc5a',
+          bug: '#7bcf00',
+          ghost: '#4e6aff',
+          steel: '#23a1bd',
+          fire: '#ff9900',
+          water: '#14a8ff',
+          grass: '#1cd80e',
+          electric: '#ffde00',
+          psychic: '#ff6c64',
+          ice: '#2ee4c6',
+          dragon: '#0076ff',
+          dark: '#5a566a',
+          fairy: '#ff76ff'
+        }
       }
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
     }
   },
   plugins: []
