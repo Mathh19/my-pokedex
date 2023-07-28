c5e619f0af316ff153cb8b0353a55e288934c685
Matheus <mf635097@gmail.com>
2023-07-27 21:15:24 -0300

    feat: new layout to navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { BiSearch } from 'react-icons/bi';
 
 export const Nav = () => {
   return (
@@ -16,23 +15,10 @@ export const Nav = () => {
           width={160}
           height={160}
           alt="pokedex written text icon"
+          priority
           className="max-sm:hidden"
         />
       </div>
-
-      <form className="group relative flex mr-3 w-full max-w-xs justify-end items-center">
-        <input
-          type="text"
-          placeholder="Search pokémon..."
-          className="rounded-full w-full shadow-[0px_0px_5px] text-zinc-950 px-3 py-1"
-        />
-        <button
-          type="submit"
-          className="absolute outline-1 right-0 shadow-[0px_0px_5px_black] flex text-xl justify-center items-center cursor-pointer w-11 h-11 rounded-full bg-black group-focus-within:bg-neutral-900 group-focus-within:outline"
-        >
-          <BiSearch />
-        </button>
-      </form>
     </nav>
   );
 };
