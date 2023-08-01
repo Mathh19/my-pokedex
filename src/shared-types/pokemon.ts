d9c27c915a67bbcf135249421cdddafce11cfa63
Matheus <mf635097@gmail.com>
2023-07-31 21:32:22 -0300

    feat: add shared types for pokemon data and pokemon types

---
+++ src/shared-types/pokemon.ts
@@ -0,0 +1,32 @@
+import { PokemonTypes } from './pokemonTypes';
+
+export type PokemonProps = {
+  id: number;
+  name: string;
+  order: number;
+  sprites: {
+    other: {
+      home: {
+        front_default?: string;
+      };
+    };
+  };
+  types: {
+    type: {
+      name: PokemonTypes;
+    };
+  }[];
+  moves: {
+    move: {
+      name: string;
+    };
+  }[];
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }[];
+  height: number;
+  weight: number;
+};
---
+++ src/shared-types/pokemonTypes.ts
@@ -0,0 +1,18 @@
+export type PokemonTypes =
+  | 'bug'
+  | 'dark'
+  | 'electric'
+  | 'fairy'
+  | 'fighting'
+  | 'fire'
+  | 'flying'
+  | 'ghost'
+  | 'grass'
+  | 'ground'
+  | 'ice'
+  | 'normal'
+  | 'poison'
+  | 'psychic'
+  | 'rock'
+  | 'steel'
+  | 'water';
