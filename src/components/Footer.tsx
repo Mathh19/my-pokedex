0d9597033096d0c314c112d3d2bef44d5a26cf28
Matheus <mf635097@gmail.com>
2023-08-09 14:52:32 -0300

    fix: add description to links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,7 @@ export const Footer = () => {
       <div className="w-full max-w-5xl flex justify-center text-3xl gap-20 py-5 border-t border-zinc-500">
         <a
           href="https://github.com/Mathh19/my-pokedex"
+          aria-label="link to the repository for this project"
           target="_blank"
           className="duration-200 ease-in-out hover:drop-shadow-[0_0_4px_#FAFAFA]"
           rel="noreferrer"
@@ -15,6 +16,7 @@ export const Footer = () => {
         <a
           href="https://www.linkedin.com/in/matheus-freitas-629557232/"
           target="_blank"
+          aria-label="link to website developer linkedin profile"
           className="duration-200 ease-in-out hover:drop-shadow-[0_0_4px_#FAFAFA]"
           rel="noreferrer"
         >
