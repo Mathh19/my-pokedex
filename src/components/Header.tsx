24c5f2d55fa724f4d11bb1da4ae0a0122f9ac0eb
Matheus <mf635097@gmail.com>
2023-08-22 22:01:24 -0300

    fix: fix logo text position on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,17 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className="bg-header-primary w-full drop-shadow-md py-2 px-4 flex justify-between items-center">
-      <Link href="/">
-        <div className="flex gap-1">
-          <Image
-            src="/assets/imgs/logo-pokeball.svg"
-            width={64}
-            height={64}
-            alt="pokeball icon"
-          />
-          <p className="inline-block font-mono text-[2.5rem]/relaxed font-medium tracking-wide">
-            Pokédex
-          </p>
-        </div>
+    <header className="bg-header-primary w-full flex drop-shadow-md py-2 px-4">
+      <Link href="/" className="flex gap-1 max-sm:items-center">
+        <Image
+          src="/assets/imgs/logo-pokeball.svg"
+          width={64}
+          height={64}
+          alt="pokeball icon"
+        />
+        <p className="font-mono text-[2.5rem]/tight font-medium tracking-wide">
+          Pokédex
+        </p>
       </Link>
     </header>
   );
